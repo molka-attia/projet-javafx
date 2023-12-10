c2ce87908f374e97c5034f4b1435e0fad8bdf76b
molka-attia <81327438+molka-attia@users.noreply.github.com>
2023-12-10 19:05:02 +0100

    Add files via upload

---
+++ Technicien.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author molka
+ */
+public final class Technicien extends Utilisateur{
+
+    public Technicien(StringProperty id, StringProperty nom, StringProperty prenom, StringProperty email, StringProperty password, StringProperty tel, StringProperty poste, StringProperty type) {
+        super(id, nom, prenom, email, password, tel, poste, type);
+    }
+
+    public Technicien() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+   
+// public Technicien(String id, String nom, String prenom, String email, String password, String tel, String poste, String type, String user_img) {
+//        super(id, nom, prenom, email, password, tel, poste, type, user_img);
+//    }
+    @Override
+    public String toString() {
+        return super.toString(); 
+    }
+    
+    
+    
+}
